893f1b8e2656c35ed642d0a4af046e05714b1af4
Stewart Bryson <stewart@redpillanalytics.com>
2018-08-14 16:15:35 -0500

    Multiply UDF for Kafka

---
+++ src/main/groovy/Multiply.groovy
@@ -0,0 +1,20 @@
+import io.confluent.ksql.function.udf.*
+
+@UdfDescription(name = "multiply", description = "multiplies 2 numbers")
+class Multiply {
+
+   @Udf(description = "multiply two non-nullable INTs.")
+   public long multiply(final int v1, final int v2) {
+      return v1 * v2;
+   }
+
+   @Udf(description = "multiply two nullable BIGINTs. If either param is null, null is returned.")
+   public long multiply(final long v1, final long v2) {
+      return v1 == null || v2 == null ? null : v1 * v2;
+   }
+
+   @Udf(description = "multiply two non-nullable DOUBLEs.")
+   public double multiply(final double v1, double v2) {
+      return v1 * v2;
+   }
+}
